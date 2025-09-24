fb45ea4c1406e17824da53d44a6cbcb6adb25133
Ruben Fernandes <ruben.fer252@gmail.com>
2025-09-24 14:08:59 +0100

    update wiki: guia informativo

--- app/(paginas)/(Informações Gerais)/guia-informativo/data.ts
+++ app/(paginas)/(Informações Gerais)/guia-informativo/data.ts
@@ -1,7 +1,7 @@
 const MODIFICAR: FuseWikiDataType = {
-    title: '',
-    url: '',
-    content: [],
+    title: 'Guia Informativo Oficial',
+    url: '/guia-informativo',
+    content: ['Guia informativo da Licenciatura em Engenharia Informática'],
 };
 
 export default MODIFICAR;
--- app/(paginas)/(Informações Gerais)/guia-informativo/page.tsx
+++ app/(paginas)/(Informações Gerais)/guia-informativo/page.tsx
@@ -1,17 +1,37 @@
+import { H1 } from '@/components/typography';
 import type { Metadata } from 'next';
+import Link from 'next/link';
 
 export const metadata: Metadata = {
-    title: 'Wiki Lei - Guia Informativo Oficial',
+    title: 'Guia Informativo Oficial - Wiki Lei',
     description: '',
-    keywords: [''],
+    keywords: ['Wiki Lei'],
     openGraph: {
-        title: '',
+        title: 'Guia Informativo Oficial - Wiki Lei',
         description: '',
         url: '',
-        siteName: '',
+        siteName: 'Wiki Lei',
     },
 };
 
 export default function Page() {
-    return <div>page</div>;
+    return (
+        <div className='mx-auto p-4 max-w-3xl'>
+            <H1>Guia informativo Oficial</H1>
+            <ul className='space-y-2 my-4 mt-4 list-disc list-inside'>
+                <li>
+                    <Link
+                        href={
+                            'https://guiadoscursos.uab.pt/cursos/licenciatura-em-engenharia-informatica/'
+                        }
+                        target='_blank'
+                        className='hover:underline'
+                    >
+                        Guia informativo da Licenciatura em Engenharia
+                        Informática
+                    </Link>
+                </li>
+            </ul>
+        </div>
+    );
 }
